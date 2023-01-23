6063fad6b13645d78c764ae922c2da02d7f32a96
arborter <110191602+arborter@users.noreply.github.com>
2023-01-23 13:58:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ docs_x = []
 docs_y = []
 
 
-# Loop through the loaded json file named data. The bracket specifies what exactly in that file we are interested in exploring.
 for intents in data["intents"]:
     # The following loops through 'patterns' in intents.json
     # 'Patterns' are words identifiable by the machine and
@@ -37,3 +36,32 @@ for intents in data["intents"]:
         
     if intents['tag'] not in labels:
         labels.append(intents['tag'])
+        
+stemmer = LancasterStemmer()
+words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+words = sorted(list(set(words)))
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+for x,doc in enumerate(docs_x):
+	bag = []
+	wrds = [stemmer.stem(w) for w in doc]
+	for w in words:
+		if w in wrds:
+			bag.append(1)
+		else:
+			bag.append(0)
+	output_row = out_empty[:]
+	output_row[labels.index(docs_y[x])] = 1
+	training.append(bag)
+	output.append(output_row)
+#Converting training data into NumPy arrays
+training = np.array(training)
+output = np.array(output)
+
+#Saving data to disk
+with open("data.pickle","wb") as f:
+	pickle.dump((words, labels, training, output),f)
